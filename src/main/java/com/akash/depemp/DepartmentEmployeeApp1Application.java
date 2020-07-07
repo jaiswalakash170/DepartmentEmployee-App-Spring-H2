5c9ec8ebb0e9c225690b16bf95439672bf9bdb2f
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-07-07 15:55:22 +0530

    Initial Commit - Department Employee App backend application using SpringBoot, H2, and Spring Data JPA

---
+++ src/main/java/com/akash/depemp/DepartmentEmployeeApp1Application.java
@@ -0,0 +1,13 @@
+package com.akash.depemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentEmployeeApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentEmployeeApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/akash/depemp/DepartmentEmployeeApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.depemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentEmployeeApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
